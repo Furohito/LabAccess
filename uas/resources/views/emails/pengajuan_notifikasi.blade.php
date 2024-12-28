330d1584afe78dd1613f24e9d8a1e359c773cdc4
Abdurrahman Hakim Makarim <102575410+Furohito@users.noreply.github.com>
2024-12-28 23:15:57 +0700

    gatau

--- uas/resources/views/emails/pengajuan_notifikasi.blade.php
+++
@@ -1,4 +0,0 @@
-<p>Halo {{ $pengajuan->nama }},</p>
-<p>Terima kasih telah mengajukan peminjaman. Klik tombol di bawah untuk mengonfirmasi pengajuan Anda.</p>
-<a href="{{ $url }}" class="btn btn-primary">Konfirmasi Pengajuan</a>
-w
